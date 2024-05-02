c1636bcce423fa1a763d64cbe1719b41e44a72d9
Anastasiia Monakhova <anklopova@gmail.com>
2024-05-02 21:05:05 +0300

    base of the project

---
+++ myproject/myapp/views.py
@@ -0,0 +1,56 @@
+import time
+
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView, CreateView
+from myapp.models import Person
+from . import tasks
+
+
+class PageTitleMixin:
+    page_title = 'The Wall of Honor'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        # print(f'{context=}')
+        context['page_title'] = self.page_title
+
+        return context
+
+
+class PersonList(PageTitleMixin, ListView):
+    page_title = 'People'
+    model = Person
+    paginate_by = 3
+
+
+def send_mail(request):
+    task_id = None
+
+    print('task started', time.time())
+
+    task = tasks.send_mail_task.delay('hello from Django', 'abcde')  # celery case
+    # task = tasks.send_mail_task('hello from Django', 'abcde')  # sync case
+
+    print('django works', time.time())
+
+    try:
+        task_id = task.id
+        print('task_id', task_id)
+    except Exception as e:
+        pass
+
+    context = {
+        'task_id': task_id
+    }
+    return render(request, 'myapp/send_mail.html', context=context)
+
+
+class PersonCreate(CreateView):
+    model = Person
+    fields = '__all__'
+    success_url = '/'
+
+
+class PersonDetail(PageTitleMixin, DetailView):
+    model = Person
---
+++ myproject/myproject/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+import myapp.views as myapp_views
+from myproject import settings
+
+urlpatterns = [
+    path('', myapp_views.PersonList.as_view()),
+    path('person/create/', myapp_views.PersonCreate.as_view()),
+    path('person/<int:pk>/', myapp_views.PersonDetail.as_view()),
+    path('send/', myapp_views.send_mail),
+
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns.append(
+        path("__debug__/", include("debug_toolbar.urls"))
+    )
